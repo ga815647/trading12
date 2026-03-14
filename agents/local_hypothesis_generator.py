c325ff3e24b6a3ea5d9f28c073c4b4e126cbcd5a
unknown <ga815647@gmail.com>
2026-03-14 20:07:33 +0800

    feat: broaden strategy factory grid to 2700+ combinations

--- agents/local_hypothesis_generator.py
+++ agents/local_hypothesis_generator.py
@@ -19,37 +19,37 @@ def generate_local_factory():
     # Theme A: Institutional Following (Chips)
     # Params: threshold_a, consecutive_n, horizon_days
     theme_a_grid = {
-        "id": ["A01", "A02", "A03", "A05"],
-        "threshold_a": [100, 300, 500, 1000],
-        "consecutive_n": [3, 5, 10],
-        "horizon_days": [10, 20, 60]
+        "id": ["A01", "A02", "A03", "A04", "A05"],
+        "threshold_a": [100, 200, 400, 600, 800, 1000, 1500, 2000],
+        "consecutive_n": [1, 2, 3, 5, 7, 10],
+        "horizon_days": [10, 20, 30, 60]
     }
     
     # Theme B: Price Momentum
     # Params: bar_body_pct, consecutive_n, horizon_days
     theme_b_grid = {
         "id": ["B01", "B02", "B03", "B04", "B05"],
-        "bar_body_pct": [0.03, 0.05, 0.10],
-        "consecutive_n": [3, 5, 20],
-        "horizon_days": [5, 10, 20]
+        "bar_body_pct": [0.01, 0.02, 0.03, 0.05, 0.07, 0.10],
+        "consecutive_n": [1, 3, 5, 10, 20],
+        "horizon_days": [3, 5, 10, 20]
     }
     
     # Theme E: Mean Reversion (Oversold)
     # Params: indicator_val, consecutive_n, horizon_days
     theme_e_grid = {
-        "id": ["E01", "E02", "E03", "E05"],
-        "indicator_val": [20, 30, 40],
-        "consecutive_n": [3, 5, 10],
-        "horizon_days": [5, 10, 20]
+        "id": ["E01", "E02", "E03", "E04", "E05"],
+        "indicator_val": [15, 20, 25, 30, 35, 40, 45, 50],
+        "consecutive_n": [2, 3, 5, 10],
+        "horizon_days": [5, 10, 15, 20]
     }
     
     # Theme J: Sentiment / Smart Money
     # Params: threshold_a, indicator_val, horizon_days
     theme_j_grid = {
-        "id": ["J06", "J07", "J08", "J09"],
-        "threshold_a": [200, 500],
-        "indicator_val": [20, 40],
-        "horizon_days": [10, 20, 40]
+        "id": ["J01", "J03", "J06", "J07", "J08", "J09", "J10"],
+        "threshold_a": [100, 300, 500, 1000, 1500],
+        "indicator_val": [20, 30, 40, 50],
+        "horizon_days": [10, 20, 40, 60]
     }
 
     all_hypotheses = []
